1ac4a7ce396e028acea043989c53a9e504058513
gggy <gggyeoung@gmail.com>
2024-03-15 21:04:14 +0900

    240315

---
+++ src/com/javaex/practice03/Ex01.java
@@ -0,0 +1,116 @@
+package com.javaex.practice03;
+
+import java.util.Scanner;
+
+public class Ex01 {
+	public static void main(String[] args) {
+		System.out.println("Ex01");
+		System.out.println("while문==========\nindex = 0\nindex = 1 \nindex = 2");
+		System.out.println("for문============\norder = 0\norder = 1\norder = 2");
+		System.out.println("========== Code ==========");
+		System.out.println("While문==========");
+		int index = 0;
+		while(index < 3) {
+			System.out.println("index = " + index);
+			index++;
+		}
+		System.out.println("for문============");
+		for(int order=0; order<3; order++) {
+			System.out.println("order = " + order);
+		}
+		
+		System.out.println("\nEx02");
+		System.out.println("**");
+		System.out.println("**");
+		System.out.println("**");
+		System.out.println("========== Code ==========");
+		int x, y;
+		for (x=0; x<4; x++) {
+			for(y=0; y<2; y++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		System.out.println("\nEx03");
+		System.out.println("***");
+		System.out.println("***");
+		System.out.println("***");
+		System.out.println("========== Code ==========");
+		for(x=0; x<4; x++) {
+			for(y=2; y>=0; y--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		System.out.println("\nEx04");
+		System.out.println("1\n2\n3\n4");
+		System.out.println("====================");
+		System.out.println("10\n8\n6\n4\n2");
+		System.out.println("========== Code ==========");
+		for(int i=1; i<5; i++) {
+			System.out.println(i);
+		}
+		System.out.println("====================");
+		for(int i=10; i>0; i-=2) {
+			System.out.println(i);
+		}
+		
+		System.out.println("\nEx05");
+		System.out.println("1\n\2n\3\n4\n5");
+		System.out.println("========== Code ==========");
+		int i01=1;
+		boolean flag =true;
+		while(flag) {
+			if(i01>=5) {
+				flag = false;
+			}
+			System.out.println(i01);
+			i01+=1;
+		}
+		
+		System.out.println("\nEx06");
+		for(int i=1; i<=100; i++) {
+			if((i % 5 == 0) && (i % 7 ==0)) {
+				System.out.println(i);
+			}
+		}
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("\nEx07");
+		System.out.print("숫자를 입력하세요. >> ");
+		int num = sc.nextInt();
+		for(int i=1; i<=num; i++) {
+			for(int j=0; j<i; j++) {
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+		
+		System.out.println("\nEx08");
+		for(int i=1; i<10; i++) {
+			for(int j=2; j<10; j++) {
+				System.out.print(j + "*" + i + "=" + j*i + "\t");
+			}
+			System.out.println();
+		}
+		
+		System.out.println("\nEx10");
+		System.out.println("숫자를 입력하세요.");
+		int cnt = 1;
+		num = 0;
+		while(cnt <= 5) {
+			System.out.print("숫자 " + cnt + ": ");
+			int input = sc.nextInt();
+			if(input > num) {
+				num = input;
+			}
+			cnt++;
+		}
+		System.out.println("최대값은" + num + "입니다.");
+		
+		sc.close();
+	}
+}
---
+++ src/com/javaex/practice03/Ex11.java
@@ -0,0 +1,173 @@
+package com.javaex.practice03;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Ex11 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("\nEx11");
+		System.out.print("숫자를 입력하세요. >> ");
+		int num = sc.nextInt(), result = 0;
+		if(num % 2 != 0) {
+			for(int i=num-2; i>0; i-=2) {
+				num+=i;
+			}
+		} else {
+			for(int i=num-2; i>0; i-=2) {
+				num+=i;
+			}
+		}
+		System.out.println("결과값: " + num);
+		
+		System.out.println("\nEx12");
+		System.out.print("숫자를 입력하세요. >> ");
+		num = sc.nextInt();
+		int fac = 1;
+		for(int i=1; i<=num; i++) {
+			fac*=i;
+		}
+		System.out.println("결과값: " + fac);
+		
+		System.out.println("\nEx13");
+		System.out.print("숫자를 입력하세요. >> ");
+		num = sc.nextInt();
+		int sum = 0;
+		for (int i=0; i<=num; i++) {
+			sum+=i;
+		}
+		System.out.println("결과값: " + sum);
+		
+		System.out.println("\nEx14");
+		System.out.print("숫자를 입력하세요. >> ");
+		num = sc.nextInt();
+		sum=1;
+		for (int i=1; i<num; i++) {
+			sum += i+1;
+			System.out.print(i + "+");
+		}
+		System.out.println(num + "\n합계: " + sum);
+		
+		System.out.println("/nEx15");
+		System.out.print("숫자를 입력하세요. >> ");
+		num = sc.nextInt();
+		for (int i = 1; i<=num; i++) {
+			if(num % i ==0) {
+				System.out.println(i);
+			}
+		}
+		
+		System.out.println("\nEx16");
+		System.out.print("숫자를 입력하세요. >> ");
+		num = sc.nextInt();
+		sum = 0;
+		int cnt = 0;
+		for(int i=1; i<=num; i++) {
+			if(i % 5 == 0) {
+				sum+=i;
+				cnt++;
+			}
+		}
+		System.out.println("5의 배수의 개수: " + cnt);
+		System.out.println("5의 배수의 합: " + sum);
+		
+		System.out.println("\nEx17");
+		System.out.print("숫자를 입력하세요. >> ");
+		num = sc.nextInt();
+		for(int i=num; i>0; i--) {
+			for(int j=i; j>0; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		System.out.println("\nEx18");
+		System.out.print("숫자를 입력하세요. >> ");
+		num = sc.nextInt();
+		for(int i=num; i>0; i--) {
+			for(int j=i; j>0; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for(int i=1; i<=num; i++) {
+			for(int j=1; j<=i; j++) {
+				System.out.print("+");
+			}
+			System.out.println();
+		}
+		
+		System.out.println("\nEx19");
+		boolean flag = true;
+		int select, input, draw, money=0;
+		while (flag == true) {
+			System.out.println("\n---------------------------------");
+			System.out.println("1)예금 | 2)출금 | 3)잔고 | 4)종료");
+			System.out.println("---------------------------------");
+			System.out.print("선택 >> ");
+			select = sc.nextInt();
+			switch(select) {
+			case 1:
+				System.out.print("예금액 >> ");
+				input = sc.nextInt();
+				money += input;
+				continue;
+			case 2:
+				System.out.print("출금액 >> ");
+				draw = sc.nextInt();
+				money -= draw;
+				continue;
+			case 3:
+				System.out.println("잔고액 >> " + money);
+				continue;
+			case 4:
+				System.out.println("프로그램 종료");
+				flag = false;
+				break;
+			default:
+				System.out.println("다시 입력해 주세요.");
+				continue;
+			}
+		}
+		
+		
+		Random ran = new Random();
+		
+		System.out.println("\nEx20");
+		System.out.println("========================================");
+		System.out.println("\t[ 숫자맞추기 게임 시작 ]");
+		System.out.println("========================================");
+		num = ran.nextInt(100) + 1;	// 챗GPT 활용 - Java에서 정수 랜덤값을 출력하는 메서드를 알려줘
+		flag = false;
+		while (flag == false) {
+			System.out.println("num = " + num);
+			System.out.print("숫자 입력 >> ");
+			input = sc.nextInt();
+			
+			if (num == input) {
+				System.out.println("정답입니다!");
+				System.out.print("게임을 종료하시겠습니까? (y/n) >> ");
+				String select01 = sc.next();
+				
+				switch(select01) {
+				case "y":
+					System.out.println("========================================");
+					System.out.println("\t[ 숫자맞추기 게임 종료 ]");
+					System.out.println("========================================");
+					flag = true;
+					break;
+				case "n":
+					num = ran.nextInt(100) + 1;
+					continue;
+				}
+			} else if (num > input) {
+				System.out.println("UP!");
+			} else {
+				System.out.println("DOWN!");
+			}
+		}
+		
+		sc.close();
+	}
+}
